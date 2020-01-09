b0875b736cdda31641204a9cd66f98531ce697fd
Jose Maike Rodrigues de Sousa <maikerodrigues@cialne.com.br>
2020-01-09 17:42:03 -0300

    Settings debbug and sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
+    const teste = 'teste';
+    console.log(teste);
     return res.json({ message: 'Helo World'});
 });
 
-module.exports = routes;
+export default routes;
