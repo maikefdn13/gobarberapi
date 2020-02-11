779f3307abaaf46bcb9e7df8d8e886f909800ffd
Jose Maike Rodrigues de Sousa <maikerodrigues@cialne.com.br>
2020-02-11 17:05:19 -0300

    Deploy

--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,7 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.get('/teste', (req, res) => {
-  return res.json({ teste: 'Maike teste' });
+  return res.json({ teste: 'Maike Deploy' });
 });
 routes.post('/sessions', SessionControlle.store);
 
